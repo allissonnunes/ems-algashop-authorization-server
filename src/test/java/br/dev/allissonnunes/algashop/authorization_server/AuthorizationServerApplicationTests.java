e4bb2eb6e4701bf0f0f96fb88f779ae84f37d825
Allisson Alves Batista Nunes <allissonabn@gmail.com>
2026-07-05 23:34:24 -0300

    feat: initialize authorization server project with Spring Boot

---
+++ src/test/java/br/dev/allissonnunes/algashop/authorization_server/AuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.allissonnunes.algashop.authorization_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
